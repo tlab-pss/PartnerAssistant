5c5fce04fb88259b31729d675ea98e764bd1717f
sho <sskmy1024.y@gmail.com>
2019-09-21 10:15:25 +0900

    add environment

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module main
 go 1.13
 
 require (
+	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20190911031432-227b76d455e7 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 
 	reqManager "main/modules/requestmanager"
 )
 
 func main() {
+	err := godotenv.Load(fmt.Sprintf("envfiles/%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	ab := os.Getenv("TEST")
+	log.Println(ab)
+
 	e := echo.New()
 
 	e.GET("/:module", reqManager.Controller)
